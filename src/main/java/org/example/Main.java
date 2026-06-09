8c27abfdee045fed750339cbb04924e48dd78e82
GXVJS <m3351757@gmail.com>
2026-06-10 00:38:05 +0300

    кр АиСД вариант 9

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,28 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    public static boolean endsWith(String str, String suffix) {
+        if (suffix.length() > str.length()) {
+            return false;
+        }
+        return str.endsWith(suffix);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите первую строку: ");
+        String str = scanner.nextLine();
+
+        System.out.print("Введите вторую строку: ");
+        String suffix = scanner.nextLine();
+
+        boolean result = endsWith(str, suffix);
+        System.out.println("Результат: " + result);
+
+        scanner.close();
+    }
+}
